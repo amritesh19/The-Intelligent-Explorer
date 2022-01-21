ee03c1a352ca83e3571db9b7381e0d5460376a4a
Amritesh Anand <73696688+amritesh19@users.noreply.github.com>
2022-01-21 07:00:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# map cell to cell, add circular cell to goal point
+points_list = [(0, 1), (1, 5), (7, 4), (5, 6), (5, 4), (1, 2), (2, 3), (2, 7)]
+
+goal = 7
+trap = 2
+
+G = nx.Graph()
+G.add_edges_from(points_list)
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_edges(G, pos)
+nx.draw_networkx_labels(G, pos)
+plt.show()
+
+# how many points in graph? x points
+MATRIX_SIZE = 8
+
+# create matrix x*y
+R = np.matrix(np.ones(shape=(MATRIX_SIZE, MATRIX_SIZE)))
+R *= -1
+
+# assign zeros to paths, 100 to goal-reaching point and -10 to trap-reaching points
+for point in points_list:
+    print(point)
+    if point[1] == goal:
+        R[point] = 100
+    elif point[1] == trap:
+        R[point] = -10
+    else:
+        R[point] = 0
+
+    if point[0] == goal:
+        R[point[::-1]] = 100
+    else:
+        # reverse of point
+        R[point[::-1]] = 0
+
+# add goal point round trip
+R[goal, goal] = 100
+R[trap, trap] = -10
+
+print(R)
+# matrix([[ -1.   0.  -1.  -1.  -1.  -1.  -1.  -1.]
+#  [  0.  -1. -10.  -1.  -1.   0.  -1.  -1.]
+#  [ -1.   0. -10.   0.  -1.  -1.  -1. 100.]
+#  [ -1.  -1.   0.  -1.  -1.  -1.  -1.  -1.]
+#  [ -1.  -1.  -1.  -1.  -1.   0.  -1. 100.]
+#  [ -1.   0.  -1.  -1.   0.  -1.   0.  -1.]
+#  [ -1.  -1.  -1.  -1.  -1.   0.  -1.  -1.]
+#  [ -1.  -1.   0.  -1.   0.  -1.  -1. 100.]])
+
+Q = np.matrix(np.zeros([MATRIX_SIZE, MATRIX_SIZE]))
+
+# learning parameter
+gamma = 0.8
+
+initial_state = 1
+
+
+def available_actions(state):
+    current_state_row = R[state,]
+    av_act = np.where(current_state_row >= 0)[1]
+    return av_act
+
+
+available_act = available_actions(initial_state)
+
+
+def sample_next_action(available_actions_range):
+    next_action = int(np.random.choice(available_act, 1))
+    return next_action
+
+
+action = sample_next_action(available_act)
+
+
+def update(current_state, action, gamma):
+    max_index = np.where(Q[action,] == np.max(Q[action,]))[1]
+
+    if max_index.shape[0] > 1:
+        max_index = int(np.random.choice(max_index, size=1))
+    else:
+        max_index = int(max_index)
+    max_value = Q[action, max_index]
+
+    Q[current_state, action] = R[current_state, action] + gamma * max_value
+    print('max_value', R[current_state, action] + gamma * max_value)
+
+    if (np.max(Q) > 0):
+        return (np.sum(Q / np.max(Q) * 100))
+    else:
+        return (0)
+
+
+update(initial_state, action, gamma)
+
+# Training
+scores = []
+for i in range(700):
+    current_state = np.random.randint(0, int(Q.shape[0]))
+    available_act = available_actions(current_state)
+    action = sample_next_action(available_act)
+    score = update(current_state, action, gamma)
+    scores.append(score)
+    print('Score:', str(score))
+
+print("Trained Q matrix:")
+print(Q / np.max(Q) * 100)
+
+# Testing
+current_state = 0
+steps = [current_state]
+edgelist = []
+
+while current_state != goal:
+
+    next_step_index = np.where(Q[current_state,] == np.max(Q[current_state,]))[1]
+
+    if next_step_index.shape[0] > 1:
+        next_step_index = int(np.random.choice(next_step_index, size=1))
+    else:
+        next_step_index = int(next_step_index)
+
+    steps.append(next_step_index)
+    edgelist.append((current_state, next_step_index))
+    current_state = next_step_index
+
+print("Most efficient path:")
+print(steps)
+
+# Output graph of scores in each iteration
+plt.plot(scores)
+plt.show()
+
+# Output graph of map
+G = nx.Graph()
+G.add_edges_from(points_list)
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_edges(G, pos)
+nx.draw_networkx_labels(G, pos)
+nx.draw_networkx_edges(
+    G,
+    pos,
+    edgelist=edgelist,
+    width=4,
+    alpha=0.5,
+    edge_color="tab:green",
+)
+
+plt.show()
